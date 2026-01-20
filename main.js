0e3eaed71386dbfa15165393ccf361ed26f8b7b5
davem1988 <davidgaudreau1988@gmail.com>
2026-01-20 13:05:26 -0500

    Bump version to 1.0.2

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const { app, BrowserWindow } = require("electron");
 const { autoUpdater } = require("electron-updater");
+const log = require("electron-log");
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = "info";
 const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 400,
     height: 150,
+    icon: path.join(__dirname, "build/icon.png"),
     resizable: false,
     frame: false,
     transparent: true,
